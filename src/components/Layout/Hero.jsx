4ca2e3f327fcc94f2d9cbddd5dc5742463d31569
Chijioke Chika <8pluscreative@gmail.com>
2022-09-18 00:48:03 +0100

    Menu bar

--- src/components/Layout/Hero.jsx
+++ src/components/Layout/Hero.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import HeroIMG from "../../assets/HeroIMG.png";
 
 const Hero = () => {
-  return <section className="bg-silver p-20 "></section>;
+  return (
+    <section className="bg-silver  ">
+      <div></div>
+      <div>
+        <img src={HeroIMG} alt="Hero" />
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
