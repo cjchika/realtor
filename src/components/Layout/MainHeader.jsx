be7cbf6e00abf0f533f2043f3dc2fed5510558d6
Chijioke Chika <8pluscreative@gmail.com>
2022-09-18 00:10:04 +0100

    Menu bar

--- src/components/Layout/MainHeader.jsx
+++ src/components/Layout/MainHeader.jsx
@@ -1,27 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Logo from "../../assets/Logo.png";
 import { TiThMenu } from "react-icons/ti";
+import { CgClose } from "react-icons/cg";
 
 const MainHeader = () => {
-  const bigScreen = (
-    <div>
-      <div className="w-28  ">
-        <img src={Logo} alt="macho-logo" className="w-full " />
+  const [isMobileMenu, setIsMobileMenu] = useState(false);
+
+  const menuHandler = () => {
+    setIsMobileMenu((isMobileMenu) => !isMobileMenu);
+  };
+
+  const mobileMenu = (
+    <div className="w-11/12 max-w-2xl mt-4">
+      <ul className=" flex-col px-6 text-ash  ">
+        <li className="py-3">
+          <NavLink to="/home" activeClassName="text-blue">
+            Home
+          </NavLink>
+        </li>
+        <li className="py-3">
+          <NavLink to="/listings" activeClassName="text-blue">
+            Properties
+          </NavLink>
+        </li>
+        <li className="py-3">
+          <NavLink to="/agent" activeClassName="text-blue">
+            Agents
+          </NavLink>
+        </li>
+        <li className="py-3">
+          <NavLink to="/blog" activeClassName="text-blue">
+            Blog
+          </NavLink>
+        </li>
+      </ul>
+      <div className="py-3">
+        <button className="ml-6 bg-blue text-white font-bold text-xs p-3 px-3 rounded-lg shadow-md ">
+          Get Started
+        </button>
       </div>
-      <button>
-        <TiThMenu className="text-5xl p-2 " />
-      </button>
     </div>
   );
 
   return (
-    <header className="w-full fixed bg-white top-0 p-4 px-10 lg:px-20 ">
-      <nav className="lg:flex items-center justify-between px-6  font-Poppins">
-        <div className="w-28 ">
-          <img src={Logo} alt="macho-logo" className="w-full " />
+    <header className="w-full fixed bg-white top-0 p-4 px-6 lg:px-20 ">
+      <nav className="flex justify-end lg:flex items-center justify-between px-6  font-Poppins">
+        <div className="flex  items-center ">
+          <div className="w-28 ">
+            <img src={Logo} alt="macho-logo" className="w-full " />
+          </div>
         </div>
-        <ul className="flex px-6 text-ash invisible lg:visible ">
+        <ul className=" hidden lg:flex px-6 text-ash  ">
           <li>
             <NavLink to="/home" activeClassName="text-blue" className="px-4">
               Home
@@ -47,15 +77,22 @@ const MainHeader = () => {
             </NavLink>
           </li>
         </ul>
-        <div className="pl-18">
-          <button className=" invisible lg:visible bg-blue text-white font-bold text-xs p-3 px-5  hover:text-blue hover:bg-white rounded-lg shadow-md ">
+        <div className="pl-18 hidden lg:flex ">
+          <button className=" bg-blue text-white font-bold text-xs p-3 px-5 hover:text-blue hover:bg-white hover:outline hover:outline-blue rounded-lg shadow-md ">
             Get Started
           </button>
-          <button>
-            <TiThMenu className="text-5xl p-2 lg:hidden " />
+        </div>
+        <div className="lg:hidden">
+          <button onClick={menuHandler}>
+            {!isMobileMenu ? (
+              <TiThMenu className="text-5xl p-2" />
+            ) : (
+              <CgClose className="text-5xl p-2" />
+            )}
           </button>
         </div>
       </nav>
+      {isMobileMenu && mobileMenu}
     </header>
   );
 };
