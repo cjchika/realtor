f4b6b1c316cd78f66a32887daef2a9134b3b8a8f
Chijioke Chika <8pluscreative@gmail.com>
2022-09-18 14:14:15 +0100

    Hero Section

--- src/components/Layout/Hero.jsx
+++ src/components/Layout/Hero.jsx
@@ -3,10 +3,17 @@ import HeroIMG from "../../assets/HeroIMG.png";
 
 const Hero = () => {
   return (
-    <section className="bg-silver  ">
-      <div></div>
-      <div>
-        <img src={HeroIMG} alt="Hero" />
+    <section className="mb-20">
+      <div className="bg-silver h-screen flex justify-between">
+        <div className="w-auto pl-24 pt-20">
+          <h2>Time to Meet Your</h2>
+          <h1 className="text-6xl text-blue font-semibold tracking-widest font-Poppins">
+            New Home
+          </h1>
+        </div>
+        <div className="w-auto">
+          <img src={HeroIMG} alt="Hero" className="max-h-screen" />
+        </div>
       </div>
     </section>
   );
